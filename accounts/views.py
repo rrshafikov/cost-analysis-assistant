0c85a4af246c81d5f1badbecf699ab1f6945a7d2
Ruslan Shafikov <rs31113@yandex.ru>
2025-12-07 11:13:02 +0300

    feat: add RegisterView with auto login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+class RegisterView(FormView):
+    template_name = "accounts/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("dashboard")
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("dashboard")
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
