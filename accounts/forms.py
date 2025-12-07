4c6038f3f20491c81ce15cad3feac207f5c49500
Ruslan Shafikov <rs31113@yandex.ru>
2025-12-07 11:12:31 +0300

    feat: add RegisterForm for user sign up

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        required=False,
+        help_text="Необязательно, можно оставить пустым.",
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
